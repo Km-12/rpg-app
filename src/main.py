55abf411093d3a6d70f6c5c87407c67a8e424f1b
Km-12 <79846225+Km-12@users.noreply.github.com>
2026-02-10 23:32:15 +0900

    update.

--- src/main.py
+++ src/main.py
@@ -18,8 +18,12 @@ class Player:
         # 鍵を取得数
         self.key_count = 0
 
-        # 速度
+        # 移動速度
         self.speed = 2
+        # 移動向き
+        self.direction = "down"
+        # 移動状態
+        self.is_moving = False
 
     def move_check(self, x, y, mapmode, map_x):
         # 移動不可なタイルは移動できないように判定
@@ -46,25 +50,53 @@ class Player:
         return pyxel.tilemaps[0].pget(tile_x, tile_y) in self.target_tiles_up
 
     def update(self, mapmode, map_x):
-        # 移動
+        self.is_moving = False
+        # 右移動
         if pyxel.btn(pyxel.KEY_RIGHT):
+            self.direction = "right"
             if self.move_check(self.player_pos[0]+16, self.player_pos[1], mapmode, map_x):
                 self.player_pos[0] += self.speed
-            
+                self.is_moving = True
+        # 左移動            
         elif pyxel.btn(pyxel.KEY_LEFT):
+            self.direction = "left"
             if self.move_check(self.player_pos[0]-4, self.player_pos[1], mapmode, map_x):
                 self.player_pos[0] -= self.speed
-
+                self.is_moving = True
+        # 上移動
         elif pyxel.btn(pyxel.KEY_UP):
+            self.direction = "up"
             if self.move_check(self.player_pos[0], self.player_pos[1]-4, mapmode, map_x):
                 self.player_pos[1] -= self.speed
-
+                self.is_moving = True
+        # 下移動
         elif pyxel.btn(pyxel.KEY_DOWN):
+            self.direction = "down"
             if self.move_check(self.player_pos[0], self.player_pos[1]+16, mapmode, map_x):
                 self.player_pos[1] += self.speed
+                self.is_moving = True
 
     def draw(self):
+        # 立ち絵
+        if not self.is_moving:
             pyxel.blt(self.player_pos[0] , self.player_pos[1] , 0, 0, 48, self.w, self.h, 13)
+            return
+        
+        # 歩行
+        frame_u = 64 if (pyxel.frame_count // 8) % 2 == 0 else 80
+
+        # 向きごとに座標を設定
+        if self.direction == "down":
+            v = 0
+        elif self.direction == "right":
+            v = 16
+        elif self.direction == "left":
+            v = 32
+        elif self.direction == "up":
+            v = 48
+
+        pyxel.blt(self.player_pos[0], self.player_pos[1], 0, v, frame_u, self.w, self.h, 13)
+        
 
 class Key:
     # Keyの初期化
@@ -79,6 +111,19 @@ class Key:
     def draw(self):
         if not self.taken:
             pyxel.blt(self.x, self.y, 0, 48, 24, self.w, self.h, 13)
+
+class FakeKey:
+    # FakeKeyの初期化
+    def __init__(self, x, y):
+        # 初期位置
+        self.x = x
+        self.y = y
+        self.w = 16
+        self.h = 16
+        self.taken = False
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, 48, 40, self.w, self.h, 13)
 
 class Box:
     # Boxの初期化
@@ -119,6 +164,7 @@ class App:
         self.player = Player(8, 32)
 
         self.key = Key(8, 8)
+        self.fake_key = FakeKey(228, 98)
         self.box = Box(8, 200)
         self.fake_box = FakeBox(8, 136)
 
@@ -181,9 +227,17 @@ class App:
                 self.player.key_count += 1
                 self.key.taken = True
 
-            # 宝箱の当たり判定
+            # 当たり宝箱の当たり判定
             if self.player.key_count == 1 and self.check_collision(self.player, self.box):
                 self.state = "clear"
+
+            # 偽宝箱の当たり判定
+            if self.check_collision(self.player, self.fake_box):
+                self.state = "over"
+
+            # 偽鍵の当たり判定
+            if self.check_collision(self.player, self.fake_key):
+                self.state = "over"
 
             # ステージ１、ステージ２で階段に乗った場合
             if self.mapmode == "stage_one":
@@ -222,6 +276,19 @@ class App:
                 elif 105 <= mx <=132 and 80 <= my <= 88:
                     pyxel.quit()
 
+        # --- ゲームオーバー画面 ---
+        elif self.state == "over":
+            # マウスカーソルを表示する
+            pyxel.mouse(True)
+            
+            if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+                # もういちど
+                if 30 <= mx <= 68 and 80 <= my <= 88:
+                    self.reset_game()
+                # おわる
+                elif 105 <= mx <=132 and 80 <= my <= 88:
+                    pyxel.quit()
+
     def draw(self):
         pyxel.cls(0)
 
@@ -236,11 +303,16 @@ class App:
             if self.mapmode == "stage_two":
               self.key.draw()
               self.box.draw()
+              self.fake_key.draw()
               self.fake_box.draw()
 
         if self.state == "clear":
             pyxel.text(100, 40, "ゲーム クリア！", 10, self.font)
             pyxel.text(30, 80, "もういちど", 7, self.font)
             pyxel.text(105, 80, "やめる", 7, self.font)
-        
+   
+        if self.state == "over":
+            pyxel.text(100, 40, "ゲーム オーバー！", 10, self.font)
+            pyxel.text(30, 80, "もういちど", 7, self.font)
+            pyxel.text(105, 80, "やめる", 7, self.font)     
 App()
